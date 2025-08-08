fbc870e234af89ba46921742ccad53800f884f73
Julio <juliocesar.gm6000@gmail.com>
2025-08-07 21:14:33 -0300

    Criando a classe de context e a string de conexao

---
+++ APICatalogo/APICatalogo/Program.cs
@@ -0,0 +1,24 @@
+// essa classe WebApplication permite que eu faça toda a configuração que eu preciso da API, consigo instanciar o configuration,
+// configurar o container de injeção de dependencia, configurar o pipeline do aplicativo configurando os middlewares...
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
